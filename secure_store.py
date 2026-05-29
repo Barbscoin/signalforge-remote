8f10338822b3c2b26e3752cc9a18df009cbbde28
PRIME PULSE <barbcoin@gmail.com>
2026-05-29 17:36:00 -0400

    Clean repository and remove local artifacts

--- secure_store.py
+++ secure_store.py
@@ -1,84 +1,43 @@
-import json
-from pathlib import Path
 from cryptography.fernet import Fernet
+import json
 import os
 
-
-class SecureStore:
-    """Encrypted credential storage"""
-    
-    def __init__(self, store_path="credentials.json", key_path=".store_key"):
-        self.store_path = Path(store_path)
-        self.key_path = Path(key_path)
-        self.cipher = self._get_cipher()
-    
-    def _get_cipher(self):
-        """Get or create encryption cipher"""
-        if self.key_path.exists():
-            with open(self.key_path, 'rb') as f:
-                key = f.read()
-        else:
-            key = Fernet.generate_key()
-            with open(self.key_path, 'wb') as f:
-                f.write(key)
-        return Fernet(key)
-    
-    def _load_store(self):
-        """Load credentials from file"""
-        if self.store_path.exists():
-            try:
-                with open(self.store_path, 'r') as f:
-                    return json.load(f)
-            except:
-                return {}
-        return {}
-    
-    def _save_store(self, data):
-        """Save credentials to file"""
-        with open(self.store_path, 'w') as f:
-            json.dump(data, f, indent=2)
-    
-    def encrypt_password(self, password: str) -> str:
-        """Encrypt a password"""
-        return self.cipher.encrypt(password.encode()).decode()
-    
-    def decrypt_password(self, encrypted: str) -> str:
-        """Decrypt a password"""
-        return self.cipher.decrypt(encrypted.encode()).decode()
-    
-    def store_credential(self, name: str, credential_type: str, value: str):
-        """Store a credential (password, API key, SSH key, etc)"""
-        data = self._load_store()
-        encrypted = self.encrypt_password(value)
-        if name not in data:
-            data[name] = {}
-        data[name][credential_type] = encrypted
-        self._save_store(data)
-    
-    def get_credential(self, name: str, credential_type: str) -> str:
-        """Retrieve a credential"""
-        data = self._load_store()
-        if name in data and credential_type in data[name]:
-            encrypted = data[name][credential_type]
-            return self.decrypt_password(encrypted)
-        return None
-    
-    def delete_credential(self, name: str, credential_type: str = None):
-        """Delete a credential"""
-        data = self._load_store()
-        if name in data:
-            if credential_type:
-                if credential_type in data[name]:
-                    del data[name][credential_type]
-            else:
-                del data[name]
-        self._save_store(data)
-    
-    def list_credentials(self) -> dict:
-        """List all stored credential names"""
-        data = self._load_store()
-        return {name: list(creds.keys()) for name, creds in data.items()}
+KEY_FILE = "secret.key"
+STORE_FILE = "devices.enc"
 
 
-# Global instance
-secure_store = SecureStore()
+def generate_key():
+    key = Fernet.generate_key()
+    with open(KEY_FILE, "wb") as f:
+        f.write(key)
+    return key
+
+
+def load_key():
+    if not os.path.exists(KEY_FILE):
+        return generate_key()
+    return open(KEY_FILE, "rb").read()
+
+
+def get_cipher():
+    return Fernet(load_key())
+
+
+def save_devices(devices: list):
+    cipher = get_cipher()
+    data = json.dumps(devices).encode()
+    encrypted = cipher.encrypt(data)
+
+    with open(STORE_FILE, "wb") as f:
+        f.write(encrypted)
+
+
+def load_devices():
+    if not os.path.exists(STORE_FILE):
+        return []
+
+    cipher = get_cipher()
+    data = open(STORE_FILE, "rb").read()
+    decrypted = cipher.decrypt(data)
+
+    return json.loads(decrypted.decode())
